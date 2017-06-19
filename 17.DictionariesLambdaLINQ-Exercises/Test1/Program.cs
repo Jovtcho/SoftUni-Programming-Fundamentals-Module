aecea933e58cb2ac1afc4f164c9fb428830086d9
Yovtcho <yovtcho.yovtchev@gmail.com>
2017-06-19 12:26:47 +0300

    Upload homework 17, problem 11 DragonArmy

--- 17.DictionariesLambdaLINQ-Exercises/Test1/Program.cs
+++ 17.DictionariesLambdaLINQ-Exercises/Test1/Program.cs
@@ -12,17 +12,31 @@ namespace Test1
         {
             List<string> inputLine = Console.ReadLine().Split().ToList();
 
-            string num = null;
-            double num1 = null;
+            //string num = null;
+            //double num1 = null;
 
-            var nums = inputLine.Where((str, index) => index % 2 == 0).Select(str => str).Select(str => int.Parse(str)).ToList();
-            var material = inputLine.Where((str, index) => index % 2 == 1).Select(str => str).ToList();
+            //var nums = inputLine.Where((str, index) => index % 2 == 0).Select(str => str).Select(str => int.Parse(str)).ToList();
+            //var material = inputLine.Where((str, index) => index % 2 == 1).Select(str => str).ToList();
 
+            //List<string> removedElement = inputLine.RemoveRange(inputLine.Count - 2, 2);        
 
-            //List<string> removedElement = inputLine.RemoveRange(inputLine.Count - 2, 2);
+            var defaultStats = new List<int> { 45, 250, 10 };
+
+            var stats = new List<int>();
+
+            for (int i = 0; i < inputLine.Count; i++)
+            {
+                if (inputLine[i] == "null")
+                {
+                    stats.Add(defaultStats[i]);
+                }
+                else
+                {
+                    stats.Add(int.Parse(inputLine[i]));
+                }
+            }
 
             Console.WriteLine();
-
         }
     }
 }
